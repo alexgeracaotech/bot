4aa5754669292c94b3cb2e92547b9d49cbc321db
Alex Bessa <alexbessa.dev@gmail.com>
2025-10-02 20:35:51 -0300

    add messege auto

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+// // MÉTODOS SIMPLES
+
+// // unshift
+// numeros.unshift(0); // adiciona no início
+// // push
+// numeros.push(6); // adiciona no final
+// // shift
+// numeros.shift(); // remove no início
+// // pop
+// numeros.pop(); // remove no final
+
+// MÉTODOS COMPLEXOS
+
+// map
+const numeros = [1, 2, 3, 4, 5];
+
+const dobrados = numeros.map(n => n * 2);
+
+console.log(dobrados);
